54d25571038324094ce9dbd87b088705b50c48d1
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-03-11 22:06:02 +0800

    3306 done

---
+++ 3306.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int getIndex(char c)
+    {
+        switch (c)
+        {
+        case 'a':
+            return 0;
+        case 'e':
+            return 1;
+        case 'i':
+            return 2;
+        case 'o':
+            return 3;
+        case 'u':
+            return 4;
+        default: // consonant
+            return 5;
+        }
+    }
+    long long atLeastKConsonant(string word, int k)
+    {
+        int n = word.size();
+        int i = 0;
+        long long num = 0;
+        int charCount[6]{0};
+        for (int j = 0; j < n; j++)
+        {
+            charCount[getIndex(word[j])]++;
+            int endMargin = n - j;
+            while (charCount[0] && charCount[1] && charCount[2] && charCount[3] && charCount[4] && charCount[5] >= k)
+            {
+                num += endMargin;
+                charCount[getIndex(word[i])]--;
+                i++;
+            }
+        }
+        return num;
+    }
+    long long countOfSubstrings(string word, int k)
+    {
+        return atLeastKConsonant(word, k) - atLeastKConsonant(word, k + 1);
+    }
+};
+
+string word = "ieaouqqieaouqq";
+int k = 1;
+
+int main()
+{
+    Solution sol;
+    int ans = sol.countOfSubstrings(word, k);
+    printf("ans = %d\n", ans); // 3
+    return 0;
+}
