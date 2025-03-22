106151b27515b9ff32910531cff3c69eb7d4c238
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-03-23 03:00:49 +0800

    3306 (140 ms --> 80 ms)

--- 3306.cpp
+++ 3306.cpp
@@ -1,59 +1,86 @@
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Solution
 {
 public:
-    int getIndex(char c)
-    {
-        switch (c)
-        {
-        case 'a':
-            return 0;
-        case 'e':
-            return 1;
-        case 'i':
-            return 2;
-        case 'o':
-            return 3;
-        case 'u':
-            return 4;
-        default: // consonant
-            return 5;
-        }
-    }
-    long long atLeastKConsonant(string word, int k)
+    long long countOfSubstrings(string word, int k)
     {
         int n = word.size();
         int i = 0;
         long long num = 0;
-        int charCount[6]{0};
+        int charCount[128]{0};
+        int consonantCount = 0;
+        bool isVowel[128]{false};
+        isVowel['a'] = true;
+        isVowel['e'] = true;
+        isVowel['i'] = true;
+        isVowel['o'] = true;
+        isVowel['u'] = true;
+        queue<int> consonantQueue;
+        int firstRightConsonant = -1;
         for (int j = 0; j < n; j++)
         {
-            charCount[getIndex(word[j])]++;
-            int iOld = i;
-            while (charCount[0] && charCount[1] && charCount[2] && charCount[3] && charCount[4] && charCount[5] >= k)
+            if (!isVowel[word[j]])
             {
-                charCount[getIndex(word[i])]--;
+                consonantQueue.push(j);
+                if (consonantCount == k)
+                {
+                    while (i < consonantQueue.front() + 1)
+                    {
+                        charCount[word[i]]--;
+                        i++;
+                    }
+                    consonantQueue.pop();
+                }
+                else
+                {
+                    consonantCount++;
+                }
+            }
+            charCount[word[j]]++;
+            int iOld = i;
+            while (charCount['a'] && charCount['e'] && charCount['i'] &&
+                   charCount['o'] && charCount['u'] && consonantCount == k)
+            {
+                charCount[word[i]]--;
+                consonantCount -= !isVowel[word[i]];
                 i++;
             }
-            num += (i - iOld) * (n - j);
+            if (i > iOld)
+            {
+                if (consonantCount == k - 1)
+                {
+                    consonantQueue.pop();
+                }
+                int jNew = j + 1;
+                if (firstRightConsonant == -1 || jNew > firstRightConsonant)
+                {
+                    while (jNew < n && isVowel[word[jNew]])
+                    {
+                        jNew++;
+                    }
+                    firstRightConsonant = jNew;
+                }
+                else
+                {
+                    jNew = firstRightConsonant;
+                }
+                num += (i - iOld) * (jNew - j);
+            }
         }
         return num;
     }
-    long long countOfSubstrings(string word, int k)
-    {
-        return atLeastKConsonant(word, k) - atLeastKConsonant(word, k + 1);
-    }
 };
 
-string word = "ieaouqqieaouqq";
-int k = 1;
+string word = "buiaceobubuufaaofoaooeief";
+int k = 2;
 
 int main()
 {
     Solution sol;
     int ans = sol.countOfSubstrings(word, k);
-    printf("ans = %d\n", ans); // 3
+    printf("ans = %d\n", ans); // 8
     return 0;
 }
