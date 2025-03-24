87b4411a6f767b8f698ef6423c51a30ce0c66231
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-03-24 22:08:31 +0800

    using greedy is much slower (112 ms)

--- 3169.cpp
+++ 3169.cpp
@@ -2,23 +2,18 @@
 #include <unordered_map>
 using namespace std;
 
-struct Node
-{
-    int day;
-    int delta;
-};
 class Solution
 {
 private:
-    void radixSort(vector<Node> &nums)
+    void radixSort(vector<vector<int>> &nums)
     {
         int n = nums.size();
         int buckets[256]{0};
-        vector<Node> ans(n);
+        vector<vector<int>> ans(n);
         // 7-0 bit
         for (int i = 0; i < n; i++)
         {
-            buckets[nums[i].day & 255]++;
+            buckets[nums[i][0] & 255]++;
         }
         for (int i = 0; i < 255; i++)
         {
@@ -26,13 +21,13 @@ private:
         }
         for (int i = n - 1; i >= 0; i--)
         {
-            ans[--buckets[nums[i].day & 255]] = nums[i];
+            ans[--buckets[nums[i][0] & 255]] = nums[i];
         }
         fill_n(buckets, 256, 0);
         // 15-8 bit
         for (int i = 0; i < n; i++)
         {
-            buckets[(ans[i].day >> 8) & 255]++;
+            buckets[(ans[i][0] >> 8) & 255]++;
         }
         for (int i = 0; i < 255; i++)
         {
@@ -40,13 +35,13 @@ private:
         }
         for (int i = n - 1; i >= 0; i--)
         {
-            nums[--buckets[(ans[i].day >> 8) & 255]] = ans[i];
+            nums[--buckets[(ans[i][0] >> 8) & 255]] = ans[i];
         }
         // 23-16 bit
         fill_n(buckets, 256, 0);
         for (int i = 0; i < n; i++)
         {
-            buckets[(nums[i].day >> 16) & 255]++;
+            buckets[(nums[i][0] >> 16) & 255]++;
         }
         for (int i = 0; i < 255; i++)
         {
@@ -54,13 +49,13 @@ private:
         }
         for (int i = n - 1; i >= 0; i--)
         {
-            ans[--buckets[(nums[i].day >> 16) & 255]] = nums[i];
+            ans[--buckets[(nums[i][0] >> 16) & 255]] = nums[i];
         }
         // 31-24 bit
         fill_n(buckets, 256, 0);
         for (int i = 0; i < n; i++)
         {
-            buckets[(ans[i].day >> 24) & 255]++;
+            buckets[(ans[i][0] >> 24) & 255]++;
         }
         for (int i = 0; i < 255; i++)
         {
@@ -68,7 +63,7 @@ private:
         }
         for (int i = n - 1; i >= 0; i--)
         {
-            nums[--buckets[(ans[i].day >> 24) & 255]] = ans[i];
+            nums[--buckets[(ans[i][0] >> 24) & 255]] = ans[i];
         }
     }
 
@@ -76,45 +71,28 @@ public:
     int countDays(int days, vector<vector<int>> &meetings)
     {
         int n = meetings.size();
-        vector<Node> nodes(n << 1);
-        int dayBeyondCount = 0;
-        for (int i = 0; i < n; i++)
+        radixSort(meetings);
+        int ans = meetings[0][0] - 1;
+        int end = meetings[0][1] + 1;
+        for (int i = 1; i < n; i++)
         {
-            nodes[i << 1] = {meetings[i][0], 1};
-            nodes[(i << 1) + 1] = {meetings[i][1] + 1, -1};
-            dayBeyondCount += (meetings[i][1] == days);
-        }
-        radixSort(nodes);
-        int ans = nodes[0].day - 1;
-        int meetingCount = 0;
-        int i = 0;
-        int nodeCount = nodes.size() - dayBeyondCount;
-        int firstNoMeetingDay = 0;
-        while (i < nodeCount)
-        {
-            meetingCount += nodes[i].delta;
-            while (i < nodeCount - 1 && nodes[i].day == nodes[i + 1].day)
+            if (meetings[i][0] > end)
             {
-                i++;
-                meetingCount += nodes[i].delta;
-            }
-            if (meetingCount == 0)
-            {
-                firstNoMeetingDay = nodes[i].day;
+                ans += (meetings[i][0] - end);
+                end = meetings[i][1] + 1;
             }
             else
             {
-                if (firstNoMeetingDay > 0)
+                int newEnd = meetings[i][1] + 1;
+                if (newEnd > end)
                 {
-                    ans += (nodes[i].day - firstNoMeetingDay);
-                    firstNoMeetingDay = 0;
+                    end = newEnd;
                 }
             }
-            i++;
         }
-        if (firstNoMeetingDay > 0)
+        if (end <= days)
         {
-            ans += (days - firstNoMeetingDay + 1);
+            ans += (days - end + 1);
         }
         return ans;
     }
