c6c7dbea2341953cfb0b15fbe122149d9108cab0
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-05-12 01:20:28 +0800

    3547 (Weekly449 Q3) (39 ms --> 7 ms)

--- Weekly449/Q3.cpp
+++ Weekly449/Q3.cpp
@@ -1,57 +1,50 @@
 #include <vector>
 using namespace std;
 
-// TODO: use union-find, path-compression, etc...
 struct Component {
     bool isCycle;
     int nodeCount;
-    vector<int> nodes;
 };
 const int NMAX = 50000;
-int compIndexes[NMAX];
+int parents[NMAX];
 vector<Component> comp(NMAX);
 class Solution {
    public:
     long long maxScore(int n, vector<vector<int>>& edges) {
         int m = edges.size();
         for (int i = 0; i < n; i++) {
-            compIndexes[i] = -1;
+            parents[i] = -NMAX - 1;
         }
         int index = 0;
         for (int i = 0; i < m; i++) {
             int u = edges[i][0];
             int v = edges[i][1];
-            int uIndex = compIndexes[u];
-            int vIndex = compIndexes[v];
-            if (uIndex == -1) {
-                if (vIndex == -1) {
+            int uParent = getParent(u);
+            int vParent = getParent(v);
+            int uCompIndex = -parents[uParent] - 1;
+            int vCompIndex = -parents[vParent] - 1;
+            if (uCompIndex == NMAX) {
+                if (vCompIndex == NMAX) {
                     comp[index].isCycle = false;
                     comp[index].nodeCount = 2;
-                    comp[index].nodes = {u, v};
-                    compIndexes[u] = index;
-                    compIndexes[v] = index++;
+                    parents[uParent] = -(++index);
+                    parents[vParent] = uParent;
                 } else {
-                    comp[vIndex].nodeCount++;
-                    comp[vIndex].nodes.push_back(u);
-                    compIndexes[u] = vIndex;
+                    comp[vCompIndex].nodeCount++;
+                    parents[uParent] = vParent;
                 }
             } else {
-                if (vIndex == -1) {
-                    comp[uIndex].nodeCount++;
-                    comp[uIndex].nodes.push_back(v);
-                    compIndexes[v] = uIndex;
+                if (vCompIndex == NMAX) {
+                    comp[uCompIndex].nodeCount++;
+                    parents[vParent] = uParent;
                 } else {
-                    if (uIndex == vIndex) {
-                        comp[uIndex].isCycle = true;
+                    if (uCompIndex == vCompIndex) {
+                        comp[uCompIndex].isCycle = true;
                     } else {
-                        comp[uIndex].nodeCount += comp[vIndex].nodeCount;
-                        for (int j = 0; j < comp[vIndex].nodes.size(); j++) {
-                            compIndexes[comp[vIndex].nodes[j]] = uIndex;
-                        }
-                        comp[uIndex].nodes.insert(comp[uIndex].nodes.begin(),
-                                                  comp[vIndex].nodes.begin(),
-                                                  comp[vIndex].nodes.end());
-                        comp[vIndex].nodeCount = 0;
+                        comp[uCompIndex].nodeCount +=
+                            comp[vCompIndex].nodeCount;
+                        comp[vCompIndex].nodeCount = 0;
+                        parents[vParent] = uParent;
                     }
                 }
             }
@@ -97,6 +90,15 @@ class Solution {
             }
         }
         return ans;
+    }
+
+   private:
+    int getParent(int node) {
+        int p = parents[node];
+        if (p < 0) {
+            return node;
+        }
+        return getParent(p);
     }
 };
 
