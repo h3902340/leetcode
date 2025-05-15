2980ac67d0120981d6041183e8376ebd369d3fbd
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-05-15 23:17:50 +0800

    3517 (60 ms)

--- 3517.cpp
+++ 3517.cpp
@@ -1,29 +1,43 @@
-#include <_stdlib.h>
-
-#include <fstream>
+#include <string>
 
 #include "Jutil.h"
+using namespace std;
 
 #define KNRM "\x1B[0m"
 #define KRED "\x1B[31m"
 #define KGRN "\x1B[32m"
 
+// TODO: time is bad (60 ms)
 class Solution {
    public:
-    int findClosest(int x, int y, int z) {
-        int xDis = abs(x - z);
-        int yDis = abs(y - z);
-        if (xDis == yDis) {
-            return 0;
-        } else if (xDis < yDis) {
-            return 1;
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        int fre[26]{0};
+        for (int i = 0; i < n; i++) {
+            fre[s[i] - 'a']++;
         }
-        return 2;
+        string ans = "";
+        int oddIndex = -1;
+        for (int i = 0; i < 26; i++) {
+            if (fre[i] & 1) {
+                oddIndex = i;
+            }
+            for (int j = 0; j < fre[i] >> 1; j++) {
+                ans += ('a' + i);
+            }
+        }
+        if (oddIndex != -1) {
+            ans += ('a' + oddIndex);
+        }
+        for (int i = (n >> 1) - 1; i >= 0; i--) {
+            ans += ans[i];
+        }
+        return ans;
     }
 };
 
 int main() {
-    const string problemName = "3516";
+    const string problemName = "3517";
     auto begin = jtimer();
     Solution sol;
     ifstream file_in("testcases/" + problemName + "_in.txt");
@@ -32,18 +46,12 @@ int main() {
     int caseCount = 0;
     int passCount = 0;
     string line_in1;
-    string line_in2;
-    string line_in3;
     string line_out;
     while (getline(file_in, line_in1)) {
-        auto x = jread_int(line_in1);
-        getline(file_in, line_in2);
-        auto y = jread_int(line_in2);
-        getline(file_in, line_in3);
-        auto z = jread_int(line_in3);
-        auto res = sol.findClosest(x, y, z);
+        auto s = jread_string(line_in1);
+        auto res = sol.smallestPalindrome(s);
         getline(file_out, line_out);
-        auto ans = jread_int(line_out);
+        auto ans = jread_string(line_out);
         printf("Case %d", ++caseCount);
         if (res == ans) {
             passCount++;
@@ -53,11 +61,9 @@ int main() {
             allPass = false;
         }
         printf("\n%s", KNRM);
-        printf("x = %s\n", line_in1.c_str());
-        printf("y = %s\n", line_in2.c_str());
-        printf("z = %s\n", line_in3.c_str());
-        jprint_int(res, "res");
-        jprint_int(ans, "ans");
+        printf("s = %s\n", line_in1.c_str());
+        jprint_string(res, "res");
+        jprint_string(ans, "ans");
         printf("\n");
     }
     if (allPass) {
