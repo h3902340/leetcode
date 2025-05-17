d945d9ce25c730afc71092a593650b0e344c4a8a
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-05-18 03:48:45 +0800

    3510 (655 ms)

--- 3510.cpp
+++ 3510.cpp
@@ -1,222 +1,138 @@
-#include <vector>
-#include <unordered_set>
 #include <queue>
 #include <set>
+#include <unordered_set>
+#include <vector>
+
+#include "Jutil.h"
 using namespace std;
 
-// TODO: this code is unfinished, it has runtime error
-struct SegmentTree
-{
-    long min;
-    int minIndex;
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+struct Node {
+    long long sum;
+    int index;
+    int modCount;
 };
-struct Leaf
-{
-    long leftSum;
-    long rightSum;
-    int leftRange;
-    int rightRange;
-};
-class Solution
-{
-public:
-    int minimumPairRemoval(vector<int> &nums)
-    {
+
+const int NMAX = 1e5;
+long long temp[NMAX];
+int pre[NMAX];
+int nxt[NMAX];
+int modCount[NMAX - 1];
+
+class Solution {
+   public:
+    int minimumPairRemoval(vector<int>& nums) {
         int n = nums.size();
-        int size = 1;
-        while (size < n)
-        {
-            size <<= 1;
-        }
-        int leafStart = size - 1;
-        size = (size << 1) - 1;
-        vector<SegmentTree> nodes(size);
-        vector<Leaf> leaves(n - 1);
-        int decreasingPair = 0;
-        for (int i = 0; i < n - 1; i++)
-        {
-            if (nums[i] > nums[i + 1])
-            {
-                decreasingPair++;
+        auto cmp = [](Node a, Node b) {
+            if (a.sum == b.sum) {
+                return a.index > b.index;
             }
-            nodes[leafStart + i] = {nums[i] + nums[i + 1], i};
-            leaves[i] = {nums[i], nums[i + 1], 1, 1};
-        }
-        for (int i = n - 1; i < size; i++)
-        {
-            nodes[leafStart + i] = {INT64_MAX, i};
-        }
-        for (int i = leafStart - 1; i >= 0; i--)
-        {
-            SegmentTree left = nodes[(i << 1) + 1];
-            SegmentTree right = nodes[(i << 1) + 2];
-            int min = left.min;
-            int minIndex = left.minIndex;
-            if (min > right.min)
-            {
-                min = right.min;
-                minIndex = right.minIndex;
+            return a.sum > b.sum;
+        };
+        priority_queue<Node, vector<Node>, decltype(cmp)> pq(cmp);
+        int cnt = 0;
+        for (int i = 0; i < n - 1; i++) {
+            pre[i] = i - 1;
+            nxt[i] = i + 1;
+            if (nums[i] > nums[i + 1]) {
+                cnt++;
             }
-            nodes[i] = {min, minIndex};
+            Node node = {nums[i] + nums[i + 1], i, 0};
+            pq.emplace(node);
+            modCount[i] = 0;
+            temp[i] = nums[i];
         }
+        pre[n - 1] = n - 2;
+        nxt[n - 1] = -1;
+        temp[n - 1] = nums[n - 1];
         int ans = 0;
-        while (decreasingPair > 0)
-        {
-            SegmentTree node = nodes[0];
-            Leaf leaf = leaves[node.minIndex];
-            int leftIndex = node.minIndex - leaf.leftRange;
-            if (leftIndex >= 0)
-            {
-                Leaf *left = &leaves[leftIndex];
-                bool isDecreasingBefore = left->leftSum > left->rightSum;
-                left->rightSum += leaf.rightSum;
-                bool isDecreasingAfter = left->leftSum > left->rightSum;
-                if (isDecreasingBefore)
-                {
-                    if (!isDecreasingAfter)
-                    {
-                        decreasingPair--;
-                    }
-                }
-                else
-                {
-                    if (isDecreasingAfter)
-                    {
-                        decreasingPair++;
-                    }
-                }
-                left->rightRange += leaf.rightRange;
-                int c = leafStart + leftIndex;
-                nodes[c].min = left->leftSum + left->rightSum;
-            }
-            int rightIndex = node.minIndex + leaf.rightRange;
-            if (rightIndex < n - 1)
-            {
-                Leaf *right = &leaves[rightIndex];
-                bool isDecreasingBefore = right->leftSum > right->rightSum;
-                right->leftSum += leaf.leftSum;
-                bool isDecreasingAfter = right->leftSum > right->rightSum;
-                if (isDecreasingBefore)
-                {
-                    if (!isDecreasingAfter)
-                    {
-                        decreasingPair--;
-                    }
-                }
-                else
-                {
-                    if (isDecreasingAfter)
-                    {
-                        decreasingPair++;
-                    }
-                }
-                right->leftRange += leaf.leftRange;
-                int c = leafStart + rightIndex;
-                nodes[c].min = right->leftSum + right->rightSum;
-                int p = (c - 1) >> 1;
-                while (p >= 0)
-                {
-                    if (nodes[c].min < nodes[p].min)
-                    {
-                        nodes[p].min = nodes[c].min;
-                        nodes[p].minIndex = nodes[c].minIndex;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    c = p;
-                    p = (c - 1) >> 1;
-                }
-            }
-            int c = leafStart + node.minIndex;
-            nodes[c].min = INT64_MAX;
-            int p = (c - 1) >> 1;
-            while (p >= 0)
-            {
-                int minNode = (p << 1) + 1;
-                if (nodes[minNode].min > nodes[minNode + 1].min)
-                {
-                    minNode++;
-                }
-                nodes[p].min = nodes[minNode].min;
-                nodes[p].minIndex = nodes[minNode].minIndex;
-                printf("p: %d, nodes[p].minIndex: %d, nodes[p].min: %ld\n", p, nodes[p].minIndex, nodes[p].min);
-                p = (p - 1) >> 1;
-            }
-            if (leaf.leftSum > leaf.rightSum)
-            {
-                decreasingPair--;
-            }
-            if (leftIndex >= 0)
-            {
-                Leaf left = leaves[leftIndex];
-                int c = leafStart + leftIndex;
-                int p = (c - 1) >> 1;
-                while (p >= 0)
-                {
-                    if (nodes[c].min < nodes[p].min)
-                    {
-                        nodes[p].min = nodes[c].min;
-                        nodes[p].minIndex = nodes[c].minIndex;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    c = p;
-                    p = (c - 1) >> 1;
-                }
-            }
-            if (rightIndex < n - 1)
-            {
-                Leaf right = leaves[rightIndex];
-                int c = leafStart + rightIndex;
-                int p = (c - 1) >> 1;
-                while (p >= 0)
-                {
-                    if (nodes[c].min < nodes[p].min)
-                    {
-                        nodes[p].min = nodes[c].min;
-                        nodes[p].minIndex = nodes[c].minIndex;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    c = p;
-                    p = (c - 1) >> 1;
-                }
-            }
+        while (cnt > 0) {
+            Node t = pq.top();
+            pq.pop();
+            if (t.modCount != modCount[t.index]) continue;
             ans++;
-            printf("{%ld,%ld}\n", leaf.leftSum, leaf.rightSum);
+            int left = pre[t.index];
+            int right = nxt[t.index];
+            int rr = nxt[right];
+            if (temp[t.index] > temp[right]) {
+                cnt--;
+            }
+            long long mergedSum = temp[t.index] + temp[right];
+            if (left != -1) {
+                if (temp[left] > temp[t.index]) {
+                    if (temp[left] <= mergedSum) {
+                        cnt--;
+                    }
+                } else {
+                    if (temp[left] > mergedSum) {
+                        cnt++;
+                    }
+                }
+                modCount[left]++;
+                nxt[left] = right;
+                Node node = {temp[left] + mergedSum, left, modCount[left]};
+                pq.emplace(node);
+            }
+            if (rr != -1) {
+                if (temp[right] > temp[rr]) {
+                    if (mergedSum <= temp[rr]) {
+                        cnt--;
+                    }
+                } else {
+                    if (mergedSum > temp[rr]) {
+                        cnt++;
+                    }
+                }
+                modCount[right]++;
+                Node node = {mergedSum + temp[rr], right, modCount[right]};
+                pq.emplace(node);
+            }
+            temp[right] = mergedSum;
+            pre[right] = left;
         }
         return ans;
     }
 };
 
-vector<int> nums = {2, 2, -1, 3, -2, 2, 1, 1, 1, 0, -1}; // 9
-//  1. {2, 2, -1, 3, -2, 2, 1, 1, 1, 0, -1} // 5 {0, -1}
-//  2. {2, 2, -1, 3, -2, 2, 1, 1, 1, -1} // 4 {-2, 2}
-//  3. {2, 2, -1, 3, 0, 1, 1, 1, -1} // 3 {1,-1}
-//  4. {2, 2, -1, 3, 0, 1, 1, 0} // 3 {2,-1}
-//  5. {2, 1, 3, 0, 1, 1, 0} // 3 {0,1}
-//  6. {2, 1, 3, 1, 1, 0} // 3 {1,0}
-//  7. {2, 1, 3, 1, 1} // 2 {1,1}
-//  8. {2, 1, 3, 2} // 2 {2,1}
-//  9. {3, 3, 2} // 1 {3,2}
-//  {3, 5}
-
-// vector<int> nums = {5, 2, 3, 1}; // 2
-// vector<int> nums = {3, 4, 1, 1, -3, 2, 4, 3}; // 5
-// {3, 4, 1, 1, -3, 2, 4, 3}
-// {3, 4, 1, -2, 2, 4, 3}
-// {3, 4, -1, 2, 4, 3}
-// {3, 4, 1, 4, 3}
-int main()
-{
+int main() {
+    string problemName = "3510";
+    auto begin = jtimer();
     Solution sol;
-    int ans = sol.minimumPairRemoval(nums);
-    printf("ans = %d\n", ans);
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in1;
+    string line_out;
+    while (getline(file_in, line_in1)) {
+        auto nums = jread_vector(line_in1);
+        jprint_vector(nums, "nums");
+        auto res = sol.minimumPairRemoval(nums);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_int(res, "res");
+        jprint_int(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
     return 0;
 }
