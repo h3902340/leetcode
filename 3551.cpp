e5af0df596c835cc542136f9219f6159c0c448c2
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-05-18 16:55:45 +0800

    3551 (140 ms)

--- 3551.cpp
+++ 3551.cpp
@@ -7,61 +7,64 @@ using namespace std;
 #define KRED "\x1B[31m"
 #define KGRN "\x1B[32m"
 
-struct Node {
-    int i;
-    int v;
-};
+const int NMAX = 1e5;
+const int DMAX = 82;
+vector<int> fre[DMAX];
+int sums[NMAX];
+int cnt[DMAX];
 class Solution {
    public:
     int minSwaps(vector<int>& nums) {
-        vector<vector<Node>> fre(82, vector<Node>());
-        vector<int> cnt(82);
-        for (int i = 0; i < nums.size(); i++) {
+        int n = nums.size();
+        for (int i = 0; i < DMAX; i++) {
+            fre[i].clear();
+        }
+        for (int i = 0; i < n; i++) {
             int ori = nums[i];
             int s = 0;
             while (ori > 0) {
                 s += ori % 10;
                 ori /= 10;
             }
-            fre[s].push_back({i, nums[i]});
-            cnt[s]++;
+            fre[s].push_back(nums[i]);
+            sums[i] = s;
         }
         int ans = 0;
-        int k = 0;
-        for (int i = 1; i < 82; i++) {
-            std::sort(
-                fre[i].begin(), fre[i].end(),
-                [](const auto& lhs, const auto& rhs) { return lhs.v < rhs.v; });
+        cnt[0] = 0;
+        for (int i = 1; i < DMAX; i++) {
+            sort(fre[i].begin(), fre[i].end());
+            cnt[i] = fre[i].size() + cnt[i - 1];
         }
-        for (int i = 1; i < 82; i++) {
-            for (int j = 0; j < fre[i].size(); j++) {
-                if (nums[k] != fre[i][j].v) {
-                    int ori = nums[k];
-                    int s = 0;
-                    while (ori > 0) {
-                        s += ori % 10;
-                        ori /= 10;
-                    }
+        int i = 0;
+        int a = 0;
+        while (a < n) {
+            int j = 0;
+            while (j < fre[i].size()) {
+                if (nums[a] != fre[i][j]) {
+                    int s = sums[a];
                     int l = 0;
                     int r = fre[s].size() - 1;
-                    bool notFound = true;
+                    int b = -1;
                     while (l <= r) {
                         int mid = (l + r) >> 1;
-                        if (fre[s][mid].v > nums[k]) {
+                        if (fre[s][mid] > nums[a]) {
                             r = mid - 1;
-                        } else if (fre[s][mid].v < nums[k]) {
+                        } else if (fre[s][mid] < nums[a]) {
                             l = mid + 1;
                         } else {
-                            fre[s][mid].i = fre[i][j].i;
-                            notFound = false;
+                            b = cnt[s - 1] + mid;
                             break;
                         }
                     }
-                    swap(nums[k], nums[fre[i][j].i]);
+                    swap(nums[a], nums[b]);
+                    swap(sums[a], sums[b]);
                     ans++;
+                } else {
+                    j++;
+                    a++;
                 }
-                k++;
             }
+            i++;
         }
         return ans;
     }
@@ -80,6 +83,7 @@ int main() {
     string line_out;
     while (getline(file_in, line_in)) {
         auto nums = jread_vector(line_in);
+        jprint_vector(nums, "nums");
         auto res = sol.minSwaps(nums);
         getline(file_out, line_out);
         auto ans = jread_int(line_out);
