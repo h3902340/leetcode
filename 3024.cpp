b2237ede83623c16655ea14b12b2777d9ad80127
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-05-19 17:04:11 +0800

    3024 (0 ms)

---
+++ 3024.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    string triangleType(vector<int>& nums) {
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            return "none";
+        }
+        if (a != b && a != c && b != c) {
+            return "scalene";
+        }
+        if (a == b && a == c && b == c) {
+            return "equilateral";
+        }
+        return "isosceles";
+    }
+};
+
+int main() {
+    string problemName = "3024";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        auto res = sol.triangleType(nums);
+        getline(file_out, line_out);
+        auto ans = jread_string(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_string(res, "res");
+        jprint_string(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
