282304a79e47fa8605ed34de8c6e7bec75f219a4
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-05-21 01:56:43 +0800

    2071 (15 ms)

--- 2071.cpp
+++ 2071.cpp
@@ -7,12 +7,107 @@ using namespace std;
 #define KRED "\x1B[31m"
 #define KGRN "\x1B[32m"
 
-// TODO: this is not done
+const int MMAX = 5e4;
+const int BUCKET_SIZE = 256;
+const int MASK = 255;
+int q[MMAX];
+int buckets[BUCKET_SIZE];
 class Solution {
    public:
     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills,
                       int strength) {
-        return 0;
+        int n = tasks.size();
+        int m = workers.size();
+        radixSort(tasks);
+        radixSortReverse(workers);
+        int res = 0;
+        int l = 0;
+        int r = min(m, n);
+        int x = 0;
+        int y = 0;
+        int j = 0;
+        while (l <= r) {
+            int p = pills;
+            int mid = (l + r) >> 1;
+            int i = mid - 1;
+            for (; i >= 0; i--) {
+                int t = tasks[i];
+                int tpill = t - strength;
+                while (j < mid && workers[j] >= tpill) {
+                    q[y++] = workers[j++];
+                }
+                if (x == y) break;
+                if (q[x] >= t) {
+                    x++;
+                } else if (p > 0) {
+                    y--;
+                    p--;
+                } else {
+                    break;
+                }
+            }
+            x = 0;
+            if (i == -1) {
+                l = mid + 1;
+                res = mid;
+                y = 0;
+                j = 0;
+            } else {
+                r = mid - 1;
+                y += pills - p;
+            }
+        }
+        return res;
+    }
+
+   private:
+    void radixSort(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp(n);
+        int max = BUCKET_SIZE - 1;
+        for (int j = 0; j < 32; j += 8) {
+            for (int i = 0; i <= max; i++) {
+                buckets[i] = 0;
+            }
+            for (auto& e : nums) {
+                int index = (e >> j) & MASK;
+                buckets[index]++;
+                if (max < index) {
+                    max = index;
+                }
+            }
+            for (int i = 1; i <= max; i++) {
+                buckets[i] += buckets[i - 1];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                temp[--buckets[(nums[i] >> j) & MASK]] = nums[i];
+            }
+            nums.swap(temp);
+        }
+    }
+    void radixSortReverse(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp(n);
+        int max = BUCKET_SIZE - 1;
+        for (int j = 0; j < 32; j += 8) {
+            for (int i = 0; i <= max; i++) {
+                buckets[i] = 0;
+            }
+            for (auto& e : nums) {
+                int index = (e >> j) & MASK;
+                buckets[index]++;
+                if (max < index) {
+                    max = index;
+                }
+            }
+            for (int i = max; i >= 1; i--) {
+                buckets[i - 1] += buckets[i];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                temp[--buckets[(nums[i] >> j) & MASK]] = nums[i];
+            }
+            nums.swap(temp);
+        }
     }
 };
 
