146f91f46a90202991423710d9e83dae8912dbb8
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-05-23 16:45:17 +0800

    3068 (0 ms)

---
+++ 3068.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    long long maximumValueSum(vector<int>& nums, int k,
+                              vector<vector<int>>& edges) {
+        int min = INT32_MAX;
+        int c = 0;
+        long long res = 0;
+        for (auto e : nums) {
+            int diff = (e ^ k) - e;
+            res += e;
+            if (diff >= 0) {
+                res += diff;
+                c++;
+            }
+            if (abs(diff) < min) {
+                min = abs(diff);
+            }
+        }
+        if (c & 1) {
+            res -= min;
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "3068";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        getline(file_in, line_in);
+        auto edges = jread_vector2d(line_in);
+        auto res = sol.maximumValueSum(nums, k, edges);
+        getline(file_out, line_out);
+        auto ans = jread_longlong(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_longlong(res, "res");
+        jprint_longlong(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
