0922b677bba1712602fe05610c69568d7ea7f4ec
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-04 15:34:19 +0800

    1163 (0 ms)

---
+++ 1163.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    string lastSubstring(string s) {
+        int n = s.size();
+        int i = 0;
+        int j = 1;
+        int k = 0;
+        while (j + k < n) {
+            if (s[i + k] == s[j + k]) {
+                k++;
+                continue;
+            }
+            if (s[i + k] < s[j + k]) {
+                int t = i;
+                i = j;
+                j = max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+            k = 0;
+        }
+        return s.substr(i, n - i);
+    }
+};
+
+int main() {
+    string problemName = "1163";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto s = jread_string(line_in);
+        auto res = sol.lastSubstring(s);
+        getline(file_out, line_out);
+        auto ans = jread_string(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_string(res, "res");
+        jprint_string(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
