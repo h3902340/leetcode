a04f8d2fc787290ad8c4370f3dc2500d87b3d95b
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-09 01:30:32 +0800

    3578 (23 ms)

---
+++ 3578.cpp
@@ -0,0 +1,113 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+const int MOD = 1e9 + 7;
+const int N = 5e4;
+
+int dp[N];
+int qmax[N];
+int qmin[N];
+
+class Solution {
+   public:
+    int countPartitions(vector<int>& nums, int k) {
+        int n = nums.size();
+        int lmax = 0;
+        int rmax = -1;
+        int lmin = 0;
+        int rmin = -1;
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            while (lmax <= rmax && qmax[rmax] < nums[j]) {
+                rmax--;
+            }
+            while (lmin <= rmin && qmin[rmin] > nums[j]) {
+                rmin--;
+            }
+            qmax[++rmax] = nums[j];
+            qmin[++rmin] = nums[j];
+            while (qmax[lmax] - qmin[lmin] > k) {
+                if (qmax[lmax] == nums[i]) {
+                    lmax++;
+                }
+                if (qmin[lmin] == nums[i]) {
+                    lmin++;
+                }
+                i++;
+            }
+            if (j == 0) {
+                dp[j] = 1;
+                continue;
+            }
+            if (i == 0) {
+                dp[j] = dp[j - 1] + 1;
+            } else if (i == 1) {
+                dp[j] = dp[j - 1];
+            } else {
+                dp[j] = dp[j - 1] - dp[i - 2];
+                if (dp[j] < 0) {
+                    dp[j] += MOD;
+                }
+            }
+            if (dp[j] >= MOD) {
+                dp[j] -= MOD;
+            }
+            dp[j] += dp[j - 1];
+            if (dp[j] >= MOD) {
+                dp[j] -= MOD;
+            }
+        }
+        int res = dp[n - 1] - dp[n - 2];
+        if (res < 0) {
+            res += MOD;
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "3578";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        auto res = sol.countPartitions(nums, k);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_int(res, "res");
+        jprint_int(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
