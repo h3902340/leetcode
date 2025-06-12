c4e6b7920e7c7a32da36748d5abc6e703ae57592
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-12 19:33:11 +0800

    1510 (6 ms)

---
+++ 1510.cpp
@@ -0,0 +1,76 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+const int N = 1e5;
+int dp[N + 1];
+bool isInit = false;
+static void init() {
+    if (isInit) return;
+    dp[0] = 0;
+    for (int i = 1; i <= N; i++) {
+        dp[i] = 0;
+        int j = 1;
+        int j2 = 1;
+        while (j2 <= i) {
+            if (dp[i - j2] == 0) {
+                dp[i] = 1;
+                break;
+            }
+            j++;
+            j2 = j * j;
+        }
+    }
+    isInit = true;
+}
+class Solution {
+   public:
+    bool winnerSquareGame(int n) {
+        init();
+        return dp[n];
+    }
+};
+
+int main() {
+    string problemName = "1510";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto n = jread_int(line_in);
+        auto res = sol.winnerSquareGame(n);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_int(res, "res");
+        jprint_int(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
