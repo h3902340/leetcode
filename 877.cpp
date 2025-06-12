4226eb60312b69277917db9d1b9c221bcd2be859
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-12 17:31:22 +0800

    877 (0 ms)

---
+++ 877.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+// First hand always wins because he can make sure that he will pick all the
+// even index or odd index piles, because there are even number of piles and
+// there is no tie, the sum of the even piles will not be equal to the sum of
+// the odd piles, he can always choose the bigger one to win.
+class Solution {
+   public:
+    bool stoneGame(vector<int>& piles) { return true; }
+};
+
+int main() {
+    string problemName = "877";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto piles = jread_vector(line_in);
+        auto res = sol.stoneGame(piles);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_int(res, "res");
+        jprint_int(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
