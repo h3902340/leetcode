2f8d9e3d02cd4925dc028d990bf6ecf8388ba3fe
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-12 18:38:02 +0800

    1140 (3 ms)

---
+++ 1140.cpp
@@ -0,0 +1,91 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+const int N = 100;
+const int MMAX = N;
+int memo[N][N + 1];
+
+class Solution {
+   public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            piles[i] += piles[i - 1];
+        }
+        return dfs(0, 1, piles, n);
+    }
+    int dfs(int i, int M, vector<int>& piles, int n) {
+        if (memo[i][M] != -1) {
+            return memo[i][M];
+        }
+        int sum = piles[n - 1];
+        if (i > 0) {
+            sum -= piles[i - 1];
+        }
+        int res = 0;
+        for (int X = 1; X <= M * 2; X++) {
+            if (i + X == n) {
+                if (res < sum) {
+                    res = sum;
+                }
+                break;
+            }
+            int a = sum - dfs(i + X, max(M, X), piles, n);
+            if (res < a) {
+                res = a;
+            }
+        }
+        memo[i][M] = res;
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "1140";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto piles = jread_vector(line_in);
+        auto res = sol.stoneGameII(piles);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint_int(res, "res");
+        jprint_int(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
