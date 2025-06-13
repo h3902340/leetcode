c6768d8f25baf9941d616ff249058cbf8df5adca
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-13 16:02:39 +0800

    1690 (26 ms)

---
+++ 1690.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+const int N = 1000;
+// 0: alice first, 1: Bob first
+int dp[N][2];
+
+class Solution {
+   public:
+    int stoneGameVII(vector<int>& stones) {
+        int n = stones.size();
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+            dp[i][1] = stones[i];
+        }
+        for (int l = 1; l < n; l++) {
+            for (int i = 0; i + l < n; i++) {
+                int a = max(dp[i + 1][1], dp[i][1]);
+                int b = min(stones[i] + dp[i + 1][0], dp[i][0] + stones[i + l]);
+                dp[i][0] = a;
+                dp[i][1] = b;
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+int main() {
+    string problemName = "1690";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto stones = jread_vector(line_in);
+        auto res = sol.stoneGameVII(stones);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
