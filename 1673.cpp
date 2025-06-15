49875503d5b6a080305cbeb0c557b91800fa8173
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-16 02:45:44 +0800

    1673 (2 ms)

---
+++ 1673.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> res(k);
+        int r = -1;
+        for (int i = 0; i < n; i++) {
+            while (r >= 0 && res[r] > nums[i] && k < r + 1 + n - i) {
+                r--;
+            }
+            if (k == r + 1 + n - i) {
+                for (; i < n; i++) {
+                    res[++r] = nums[i];
+                }
+            } else if (r + 1 < k) {
+                res[++r] = nums[i];
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "1673";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        auto res = sol.mostCompetitive(nums, k);
+        getline(file_out, line_out);
+        auto ans = jread_vector(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
