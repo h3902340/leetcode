fa143cb9cf79cc24714920d2309d6bc2585c28fa
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-17 00:55:40 +0800

    2073 (0 ms)

---
+++ 2073.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n = tickets.size();
+        int res = k + tickets[k];
+        if (tickets[k] == 1) return res;
+        for (int i = 0; i < k; i++) {
+            tickets[i]--;
+        }
+        tickets[k]--;
+        for (int i = 0; i < n; i++) {
+            if (i == k) continue;
+            if (tickets[i] > tickets[k]) {
+                res += tickets[k];
+            } else {
+                res += tickets[i];
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "2073";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto tickets = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        auto res = sol.timeRequiredToBuy(tickets, k);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
