a6d938ff9978ff570b77168303a1f1ca244b3956
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-16 13:45:05 +0800

    2016 (0 ms)

---
+++ 2016.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    int maximumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int res = -1;
+        int cur = nums[0];
+        for (int i = 1; i < n; i++) {
+            int diff = nums[i] - cur;
+            if (diff > 0) {
+                if (res < diff) {
+                    res = diff;
+                }
+            } else if (diff < 0) {
+                cur = nums[i];
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "2016";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        auto res = sol.maximumDifference(nums);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
