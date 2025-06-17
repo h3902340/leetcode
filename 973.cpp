28532b9791e0e6815c56ea2097281b05df98c1e3
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-18 00:24:08 +0800

    973 (45 ms)

---
+++ 973.cpp
@@ -0,0 +1,108 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+struct Data {
+    int dis;
+    vector<int> point;
+};
+const int N = 1e4;
+Data arr[N];
+class Solution {
+   public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        for (int i = 0; i < n; i++) {
+            arr[i] = {distance(points[i]), points[i]};
+        }
+        kth_element(arr, 0, n - 1, k);
+        vector<vector<int>> res(k);
+        for (int i = 0; i < k; i++) {
+            res[i] = arr[i].point;
+        }
+        return res;
+    }
+    int distance(vector<int>& point) {
+        int x = point[0];
+        int y = point[1];
+        return x * x + y * y;
+    }
+    void kth_element(Data nums[N], int i, int j, int k) {
+        if (j - i + 1 == k) return;
+        srand(time(NULL));
+        int l = i + 1;
+        int r = j;
+        int mid = rand() % (j - i + 1) + i;
+        Data p = nums[mid];
+        nums[mid] = nums[i];
+        while (true) {
+            while (l <= r) {
+                if (nums[l].dis > p.dis) {
+                    swap(nums[l], nums[r]);
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+            nums[i] = nums[r];
+            nums[r] = p;
+            if (r == k) break;
+            if (r < k) {
+                i = r + 1;
+            } else {
+                j = r - 1;
+            }
+            l = i + 1;
+            r = j;
+            mid = rand() % (j - i + 1) + i;
+            p = nums[mid];
+            nums[mid] = nums[i];
+        }
+    }
+};
+
+int main() {
+    string problemName = "973";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto points = jread_vector2d(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        auto res = sol.kClosest(points, k);
+        getline(file_out, line_out);
+        auto ans = jread_vector2d(line_out);
+        printf("Case %d", ++caseCount);
+        if (anyOrderEqual(res, ans)) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
