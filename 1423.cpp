6638374fcb3c0ad748ee95e23ec5f229466fcdef
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-18 19:23:53 +0800

    1423 (0 ms)

---
+++ 1423.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int res = 0;
+        for (int i = 0; i < k; i++) {
+            res += cardPoints[i];
+        }
+        int a = res;
+        for (int i = k - 1; i >= 0; i--) {
+            a = a - cardPoints[i] + cardPoints[n - k + i];
+            if (res < a) {
+                res = a;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "1423";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto cardPoints = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        auto res = sol.maxScore(cardPoints, k);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
