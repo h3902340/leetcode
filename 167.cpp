2c023a3bb24668a8b6a9ed446efb6dde7b488348
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-20 00:54:52 +0800

    167 (0 ms)

---
+++ 167.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+        int i = 0;
+        int j = n - 1;
+        while (i + 1 != j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) break;
+            if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return {i + 1, j + 1};
+    }
+};
+
+int main() {
+    string problemName = "167";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto numbers = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto target = jread_int(line_in);
+        auto res = sol.twoSum(numbers, target);
+        getline(file_out, line_out);
+        auto ans = jread_vector(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
