87bf29758ca6e0c297db6643f47f5c6bfdcca0c8
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-06-21 01:24:26 +0800

    1953 (0 ms)

---
+++ 1953.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+#define ll long long
+class Solution {
+   public:
+    long long numberOfWeeks(vector<int>& milestones) {
+        int n = milestones.size();
+        int max = 0;
+        ll sum = 0;
+        for (auto e : milestones) {
+            if (max < e) {
+                max = e;
+            }
+            sum += e;
+        }
+        ll rest = sum - max;
+        if (max > rest + 1) {
+            return (rest << 1) + 1;
+        }
+        return sum;
+    }
+};
+
+int main() {
+    string problemName = "1953";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto milestones = jread_vector(line_in);
+        auto res = sol.numberOfWeeks(milestones);
+        getline(file_out, line_out);
+        auto ans = jread_longlong(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
