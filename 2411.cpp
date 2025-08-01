a3cb5b47363d1eb0346320f91acf9722c3804379
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-08-02 03:12:09 +0800

    2411 (6 ms)

---
+++ 2411.cpp
@@ -0,0 +1,69 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+        for (int i = 0; i < n; i++) {
+            res[i] = 1;
+            int a = nums[i];
+            int c = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                int b = nums[j] | a;
+                if (nums[j] == b) {
+                    break;
+                }
+                a = b;
+                c++;
+                nums[j] = b;
+                res[j] = c;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "2411";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        auto res = sol.smallestSubarrays(nums);
+        getline(file_out, line_out);
+        auto ans = jread_vector(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
