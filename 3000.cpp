84ba49c25ce409765a567c8f861b9a710cc8c8ec
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-08-26 14:32:15 +0800

    3000 (0 ms)

---
+++ 3000.cpp
@@ -0,0 +1,66 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        int res = 0;
+        int diagonal = 0;
+        for (auto& d : dimensions) {
+            int a = d[0] * d[0] + d[1] * d[1];
+            if (diagonal < a) {
+                diagonal = a;
+                res = d[0] * d[1];
+            } else if (diagonal == a) {
+                int b = d[0] * d[1];
+                if (res < b) {
+                    res = b;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "3000";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto dimensions = jread_vector2d(line_in);
+        auto res = sol.areaOfMaxDiagonal(dimensions);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
