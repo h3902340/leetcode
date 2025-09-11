e3ef0a1d380afb054d496571c0a45530314bf6d9
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-09-11 19:41:22 +0800

    2785 (0 ms)

---
+++ 2785.cpp
@@ -0,0 +1,80 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+const int SYM = 118;
+int cnt[SYM];
+int i;
+class Solution {
+   public:
+    string sortVowels(string s) {
+        i = 65;
+        for (auto &c : s) {
+            pick(c);
+        }
+        for (auto &c : s) {
+            put(c);
+        }
+        return s;
+    }
+    void pick(char &c) {
+        if (isVowel(c)) {
+            cnt[c]++;
+        }
+    }
+    void put(char &c) {
+        if (isVowel(c)) {
+            while (cnt[i] == 0) {
+                i++;
+            }
+            c = i;
+            cnt[i]--;
+        }
+    }
+    bool isVowel(char c) {
+        return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+               c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+};
+
+int main() {
+    string problemName = "2785";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto s = jread_string(line_in);
+        auto res = sol.sortVowels(s);
+        getline(file_out, line_out);
+        auto ans = jread_string(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
