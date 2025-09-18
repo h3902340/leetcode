778864f1a87184f8ee7f2fbc955d446ef15e811e
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-09-18 13:24:28 +0800

    3408 (90 ms)

---
+++ 3408.cpp
@@ -0,0 +1,117 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+struct Task {
+    int userId;
+    int taskId;
+    int priority;
+};
+struct Comp {
+    bool operator()(const Task& a, const Task& b) const {
+        if (a.priority == b.priority) {
+            return a.taskId < b.taskId;
+        }
+        return a.priority < b.priority;
+    }
+};
+const int TASK_ID = 1e5;
+const int USER_NIL = -1;
+int taskToUser[TASK_ID + 1];
+int taskToPriority[TASK_ID + 1];
+class TaskManager {
+   public:
+    priority_queue<Task, vector<Task>, Comp> pq;
+    TaskManager(vector<vector<int>>& tasks) {
+        for (auto& t : tasks) {
+            add(t[0], t[1], t[2]);
+        }
+    }
+
+    void add(int userId, int taskId, int priority) {
+        pq.push({userId, taskId, priority});
+        taskToUser[taskId] = userId;
+        taskToPriority[taskId] = priority;
+    }
+
+    void edit(int taskId, int newPriority) {
+        pq.push({taskToUser[taskId], taskId, newPriority});
+        taskToPriority[taskId] = newPriority;
+    }
+
+    void rmv(int taskId) { taskToUser[taskId] = USER_NIL; }
+
+    int execTop() {
+        while (!pq.empty()) {
+            Task t = pq.top();
+            pq.pop();
+            if (taskToUser[t.taskId] == USER_NIL) {
+                continue;
+            }
+            if (taskToUser[t.taskId] != t.userId) {
+                continue;
+            }
+            if (taskToPriority[t.taskId] != t.priority) {
+                continue;
+            }
+            rmv(t.taskId);
+            return t.userId;
+        }
+        return USER_NIL;
+    }
+};
+
+int main() {
+    string problemName = "3408";
+    auto begin = jtimer();
+    vector<string> cmd = {"TaskManager", "add", "edit",   "execTop",
+                          "rmv",         "add", "execTop"};
+    vector<vector<int>> tasks = {{1, 101, 10}, {2, 102, 20}, {3, 103, 15}};
+    vector<vector<int>> add = {{}, {2, 102, 20}, {}, {}, {}, {5, 105, 15}, {}};
+    vector<vector<int>> edit = {{}, {}, {102, 8}, {}, {}, {}, {}};
+    vector<int> rmv = {0, 0, 0, 0, 101, 0, 0};
+    vector<int> ans = {0, 0, 0, 3, 0, 0, 5};
+    TaskManager sol(tasks);
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    printf("Case %d\n", ++caseCount);
+    for (int i = 0; i < cmd.size(); i++) {
+        string& c = cmd[i];
+        if (c == "TaskManager") {
+            continue;
+        }
+        if (c == "add") {
+            sol.add(add[i][0], add[i][1], add[i][2]);
+            continue;
+        }
+        if (c == "edit") {
+            sol.edit(edit[i][0], edit[i][1]);
+            continue;
+        }
+        if (c == "rmv") {
+            sol.rmv(rmv[i]);
+            continue;
+        }
+        if (c == "execTop") {
+            if (sol.execTop() != ans[i]) {
+                allPass = false;
+                break;
+            }
+        }
+    }
+    if (allPass) {
+        passCount++;
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
