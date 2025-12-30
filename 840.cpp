8755772e348bc80455fdd1de61102789147f799c
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-12-30 16:22:06 +0800

    840 (0 ms)

---
+++ 840.cpp
@@ -0,0 +1,82 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        int res = 0;
+        for (int i = 1; i < row - 1; i++) {
+            for (int j = 1; j < col - 1; j++) {
+                if (grid[i][j] == 5) {
+                    if ((grid[i - 1][j - 1] & 1) == 0 &&
+                        (grid[i - 1][j + 1] & 1) == 0 &&
+                        (grid[i + 1][j - 1] & 1) == 0 &&
+                        (grid[i + 1][j + 1] & 1) == 0 &&
+                        grid[i - 1][j - 1] + grid[i + 1][j + 1] == 10 &&
+                        grid[i - 1][j + 1] + grid[i + 1][j - 1] == 10 &&
+                        grid[i - 1][j - 1] + grid[i][j - 1] +
+                                grid[i + 1][j - 1] ==
+                            15 &&
+                        grid[i - 1][j + 1] + grid[i][j + 1] +
+                                grid[i + 1][j + 1] ==
+                            15 &&
+                        grid[i - 1][j - 1] + grid[i - 1][j] +
+                                grid[i - 1][j + 1] ==
+                            15 &&
+                        grid[i + 1][j - 1] + grid[i + 1][j] +
+                                grid[i + 1][j + 1] ==
+                            15) {
+                        res++;
+                    }
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "840";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto grid = jread_vector2d(line_in);
+        auto res = sol.numMagicSquaresInside(grid);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
