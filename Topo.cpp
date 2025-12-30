23148b3bc1601e8305e202a1bbd14dbfe3ff0aa8
Jia-Ho Jian <ygmdwwyg@gmail.com>
2025-12-31 00:19:39 +0800

    topo init

--- Topo.cpp
+++ Topo.cpp
@@ -5,14 +5,17 @@ const int N = 1e5;
 int deg[N];
 int nei[N];
 int topo[N];
+void init(int n) {
+    for (int i = 0; i < n; i++) {
+        nei[i] = 0;
+    }
+}
 
 int main() {
     int n = 6;
     vector<vector<int>> edges = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {1, 5}};
     int m = edges.size();
-    for (int i = 0; i < n; i++) {
-        nei[i] = 0;
-    }
+    init(n);
     for (int i = 0; i < m; i++) {
         int u = edges[i][0];
         int v = edges[i][1];
@@ -36,6 +39,6 @@ int main() {
     for (int i = 0; i < j; i++) {
         printf("%d,", topo[i]);
     }
-    printf("\n"); // 2,0,3,4,1,
+    printf("\n");  // 2,0,3,4,1,
     return 0;
 }
