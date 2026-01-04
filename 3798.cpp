fb8079567f9e1584a3a69b7d3db516193f403b74
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-01-04 13:41:26 +0800

    3798 (0 ms)

---
+++ 3798.cpp
@@ -0,0 +1,57 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    string largestEven(string s) {
+        int n = s.size();
+        while (n > 0 && s[n - 1] == '1') {
+            s.pop_back();
+            n--;
+        }
+        return s;
+    }
+};
+
+int main() {
+    string problemName = "3798";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto s = jread_string(line_in);
+        auto res = sol.largestEven(s);
+        getline(file_out, line_out);
+        auto ans = jread_string(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
