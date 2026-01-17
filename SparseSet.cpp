de6a65b1c6b36ee626dd158a78e5d213b23ddfb9
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-01-18 00:59:14 +0800

    add SparseSet template

---
+++ SparseSet.cpp
@@ -0,0 +1,46 @@
+#include "Jutil.h"
+using namespace std;
+
+const int N = 1e5;
+const int U = 1e5;
+int sparse[U + 1];
+int dense[N];
+int num;
+void init() { num = 0; }
+bool has(int x) {
+    int idx = sparse[x];
+    return (idx < num && dense[idx] == x);
+}
+void insert(int x) {
+    int idx = sparse[x];
+    if (idx >= num || dense[idx] != x) {
+        sparse[x] = num;
+        dense[num++] = x;
+    }
+}
+void remove(int x) {
+    int idx = sparse[x];
+    if (idx < num && dense[idx] == x) {
+        if (idx < num - 1) {
+            int other = dense[num - 1];
+            sparse[other] = idx;
+            dense[idx] = other;
+        }
+        num--;
+    }
+}
+
+int main() {
+    vector<int> arr = {1, 23, 4, 33, 33, 1};
+    int n = arr.size();
+    init();
+    for (int i = 0; i < n; i++) {
+        insert(arr[i]);
+    }
+    remove(33);
+    for (int i = 0; i < num; i++) {
+        printf("%d,", dense[i]);
+    }
+    printf("\n");  // 1,23,4,
+    return 0;
+}
