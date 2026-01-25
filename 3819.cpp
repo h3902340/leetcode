bb737f2a08a4d056663f34a6374881ea3f167aba
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-01-25 12:12:17 +0800

    3819 (12 ms)

---
+++ 3819.cpp
@@ -0,0 +1,76 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+const int N = 1e5;
+int arr[N];
+
+class Solution {
+   public:
+    vector<int> rotateElements(vector<int>& nums, int k) {
+        int n = nums.size();
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                arr[j++] = nums[i];
+            }
+        }
+        if (j == 0) {
+            return nums;
+        }
+        int jj = k % j;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                nums[i] = arr[jj++];
+                if (jj == j) {
+                    jj = 0;
+                }
+            }
+        }
+        return nums;
+    }
+};
+
+int main() {
+    string problemName = "3819";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        auto res = sol.rotateElements(nums, k);
+        getline(file_out, line_out);
+        auto ans = jread_vector(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
