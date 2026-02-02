2e0c24b3c6aa75085406066240c8764605022714
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-03 01:55:22 +0800

    3824 (53 ms)

---
+++ 3824.cpp
@@ -0,0 +1,71 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+#define ll long long
+class Solution {
+   public:
+    int minimumK(vector<int>& nums) {
+        int n = nums.size();
+        int l = 1;
+        int r = 1e5;
+        int res = r;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            ll mid2 = (ll)mid * mid;
+            ll a = 0;
+            for (int i = 0; i < n; i++) {
+                a += (nums[i] + mid - 1) / mid;
+            }
+            if (a > mid2) {
+                l = mid + 1;
+            } else {
+                res = mid;
+                r = mid - 1;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "3824";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        auto res = sol.minimumK(nums);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
