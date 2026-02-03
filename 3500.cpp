71db8f66bb4f18daadaf07d0e2feae2003920dd8
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-03 19:06:50 +0800

    3500 (11 ms)

--- 3500.cpp
+++ 3500.cpp
@@ -1,29 +1,28 @@
-#include <vector>
+#include "Jutil.h"
 using namespace std;
 
-struct Line
-{
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+struct Line {
     long a;
     long b;
     Line(long a, long b) : a(a), b(b) {}
     Line() : a(0), b(0) {}
 };
-struct RationalPoint
-{
+struct RationalPoint {
     long x;
     long y;
     long d;
     RationalPoint(long x, long y, long d) : x(x), y(y), d(d) {}
     RationalPoint() : x(0), y(0), d(0) {}
 };
-class Solution
-{
-public:
-    long long minimumCost(vector<int> &nums, vector<int> &cost, int k)
-    {
+class Solution {
+   public:
+    long long minimumCost(vector<int>& nums, vector<int>& cost, int k) {
         int n = nums.size();
-        for (int i = 1; i < n; i++)
-        {
+        for (int i = 1; i < n; i++) {
             nums[i] += nums[i - 1];
             cost[i] += cost[i - 1];
         }
@@ -34,27 +33,20 @@ public:
         int lineTop = -1;
         int pointTop = -1;
         long long ans = 0;
-        for (int i = n; i >= 1; i--)
-        {
+        for (int i = n; i >= 1; i--) {
             long a = -nums[i];
             long b = (long)nums[i] * cost[i] + ans + k * (cost[n] - cost[i]);
-            if (lineTop == -1)
-            {
+            if (lineTop == -1) {
                 lines[0].a = a;
                 lines[0].b = b;
                 lineTop = 0;
-            }
-            else
-            {
-                if (pointTop != -1)
-                {
+            } else {
+                if (pointTop != -1) {
                     RationalPoint p = points[pointTop];
-                    while (a * p.x + b * p.d < p.y)
-                    {
+                    while (a * p.x + b * p.d < p.y) {
                         pointTop--;
                         lineTop--;
-                        if (pointTop == -1)
-                            break;
+                        if (pointTop == -1) break;
                         p = points[pointTop];
                     }
                 }
@@ -71,16 +63,12 @@ public:
             int r = pointTop;
             int x = cost[i - 1];
             int min = lineTop;
-            while (l <= r)
-            {
+            while (l <= r) {
                 int mid = (l + r) >> 1;
-                if (points[mid].x < x * points[mid].d)
-                {
+                if (points[mid].x < x * points[mid].d) {
                     r = mid - 1;
                     min = mid;
-                }
-                else
-                {
+                } else {
                     l = mid + 1;
                 }
             }
@@ -90,14 +78,45 @@ public:
     }
 };
 
-vector<int> nums = {648, 453, 956, 545, 348, 129, 886, 944, 327, 208, 871};
-vector<int> cost = {879, 917, 677, 565, 753, 907, 553, 756, 434, 935, 738};
-int k = 74;
-
-int main()
-{
+int main() {
+    string problemName = "3500";
+    auto begin = jtimer();
     Solution sol;
-    long long ans = sol.minimumCost(nums, cost, k);
-    printf("ans = %lld\n", ans); // 30398493
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto cost = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto k = jread_int(line_in);
+        auto res = sol.minimumCost(nums, cost, k);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
     return 0;
 }
