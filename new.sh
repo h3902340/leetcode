30c227f5fafee8f27774e56ff6adddd0c67f12d4
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-09 13:53:15 +0800

    open file automatically when create new problem

--- new.sh
+++ new.sh
@@ -2,3 +2,7 @@ cp template.txt $1.cpp
 sed -i '' "s/xxxx/$1/g" $1.cpp
 touch testcases/${1}_in.txt
 touch testcases/${1}_out.txt
+code -r $1.cpp
+code -r testcases/${1}_in.txt
+code -r testcases/${1}_out.txt
+code -r $1.cpp
