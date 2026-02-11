09ec0b5da2c64940e54fb5d97e7983f81464aa61
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-11 20:01:42 +0800

    update SparseSet template

--- SparseSet.cpp
+++ SparseSet.cpp
@@ -3,43 +3,48 @@ using namespace std;
 
 const int N = 1e5;
 const int U = 1e5;
-int sparse[U + 1];
-int dense[N];
-int num;
-void init() { num = 0; }
-bool has(int x) {
-    int idx = sparse[x];
-    return (idx < num && dense[idx] == x);
-}
-void insert(int x) {
-    int idx = sparse[x];
-    if (idx >= num || dense[idx] != x) {
-        sparse[x] = num;
-        dense[num++] = x;
+class SparseSet {
+    int sparse[U + 1];
+
+   public:
+    int dense[N];
+    int num;
+    void init() { num = 0; }
+    bool has(int x) {
+        int idx = sparse[x];
+        return (idx < num && dense[idx] == x);
     }
-}
-void remove(int x) {
-    int idx = sparse[x];
-    if (idx < num && dense[idx] == x) {
-        if (idx < num - 1) {
-            int other = dense[num - 1];
-            sparse[other] = idx;
-            dense[idx] = other;
+    void insert(int x) {
+        int idx = sparse[x];
+        if (idx >= num || dense[idx] != x) {
+            sparse[x] = num;
+            dense[num++] = x;
         }
-        num--;
     }
-}
+    void remove(int x) {
+        int idx = sparse[x];
+        if (idx < num && dense[idx] == x) {
+            if (idx < num - 1) {
+                int other = dense[num - 1];
+                sparse[other] = idx;
+                dense[idx] = other;
+            }
+            num--;
+        }
+    }
+};
 
 int main() {
     vector<int> arr = {1, 23, 4, 33, 33, 1};
     int n = arr.size();
-    init();
+    SparseSet se;
+    se.init();
     for (int i = 0; i < n; i++) {
-        insert(arr[i]);
+        se.insert(arr[i]);
     }
-    remove(33);
-    for (int i = 0; i < num; i++) {
-        printf("%d,", dense[i]);
+    se.remove(33);
+    for (int i = 0; i < se.num; i++) {
+        printf("%d,", se.dense[i]);
     }
     printf("\n");  // 1,23,4,
     return 0;
