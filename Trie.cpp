7bd24acd4b0c538e0b48e6df69e5de6a69fc0568
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-15 22:29:55 +0800

    update Trie template

--- Trie.cpp
+++ Trie.cpp
@@ -1,54 +1,75 @@
 #include "Jutil.h"
 using namespace std;
 
-const int N = 1e5;
+const int N = 4e4;
 const int C = 26;
-struct Trie {
-    int idx[C];
-};
-Trie arr[N];
-int idx;
-void init() {
-    for (int i = 0; i < C; i++) {
-        arr[0].idx[i] = 0;
+const int K = 15;
+class Trie {
+    struct Node {
+        int idx[C];
+        int cnt[C];
+    };
+    Node arr[N * K];
+    int idx;
+
+   public:
+    void init() {
+        for (int i = 0; i < C; i++) {
+            arr[0].idx[i] = 0;
+            arr[0].cnt[i] = 0;
+        }
+        idx = 1;
     }
-    idx = 1;
-}
-void add(string& s) {
-    int n = s.size();
-    int id = 0;
-    for (int i = 0; i < n; i++) {
-        int c = s[i] - 'a';
-        if (arr[id].idx[c] == 0) {
-            for (int j = 0; j < C; j++) {
-                arr[idx].idx[j] = 0;
+    void add(string& s) {
+        int n = s.size();
+        int id = 0;
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
+            if (arr[id].idx[c] == 0) {
+                for (int j = 0; j < C; j++) {
+                    arr[idx].idx[j] = 0;
+                    arr[idx].cnt[j] = 0;
+                }
+                arr[id].idx[c] = idx++;
             }
-            arr[id].idx[c] = idx++;
+            arr[id].cnt[c]++;
+            id = arr[id].idx[c];
         }
-        id = arr[id].idx[c];
     }
-}
-bool has(string& s) {
-    int n = s.size();
-    int id = 0;
-    for (int i = 0; i < n; i++) {
-        int c = s[i] - 'a';
-        if (arr[id].idx[c] == 0) {
-            return false;
+    void remove(string& s) {
+        int n = s.size();
+        int id = 0;
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
+            arr[id].cnt[c]--;
+            id = arr[id].idx[c];
         }
-        id = arr[id].idx[c];
     }
-    return true;
-}
+    bool has(string& s) {
+        int n = s.size();
+        int id = 0;
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
+            if (arr[id].cnt[c] == 0) {
+                return false;
+            }
+            id = arr[id].idx[c];
+        }
+        return true;
+    }
+};
+Trie t;
 
 int main() {
     string a = "hello";
     string b = "world";
     string c = "hell";
-    init();
-    add(a);
-    printf("has(%s):%d\n", c.c_str(), has(c));  // has("hell"):1
-    printf("has(%s):%d\n", a.c_str(), has(a));  // has("hello"):1
-    printf("has(%s):%d\n", b.c_str(), has(b));  // has("world"):0
+    t.init();
+    t.add(a);
+    printf("has(%s):%d\n", c.c_str(), t.has(c));  // has("hell"):1
+    printf("has(%s):%d\n", a.c_str(), t.has(a));  // has("hello"):1
+    printf("has(%s):%d\n", b.c_str(), t.has(b));  // has("world"):0
+    t.remove(a);
+    printf("has(%s):%d\n", a.c_str(), t.has(a));  // has("hello"):0
     return 0;
 }
