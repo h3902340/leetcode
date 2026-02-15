c88738458f805b7294d6f821059182517613e4b1
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-15 09:40:09 +0800

    3840 (5 ms)

---
+++ 3840.cpp
@@ -0,0 +1,69 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+#define ll long long
+class Solution {
+   public:
+    long long rob(vector<int>& nums, vector<int>& colors) {
+        int n = nums.size();
+        ll a = nums[0];
+        ll b = 0;
+        for (int i = 1; i < n; i++) {
+            if (colors[i - 1] == colors[i]) {
+                ll t = b;
+                b = max(a, t);
+                a = t + nums[i];
+            } else {
+                ll t = b;
+                b = max(a, t);
+                a = max(a, t) + nums[i];
+            }
+        }
+        return max(a, b);
+    }
+};
+
+int main() {
+    string problemName = "3840";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        getline(file_in, line_in);
+        auto colors = jread_vector(line_in);
+        auto res = sol.rob(nums, colors);
+        getline(file_out, line_out);
+        auto ans = jread_longlong(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
