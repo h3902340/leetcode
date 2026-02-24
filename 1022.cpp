6bebe9611064f3df67a343d9b55c41fc0b65319f
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-24 10:36:12 +0800

    1022 (0 ms)

---
+++ 1022.cpp
@@ -0,0 +1,64 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    int sumRootToLeaf(TreeNode* root) { return f(root, 0); }
+    int f(TreeNode* root, int v) {
+        v = (v << 1) | root->val;
+        if (root->left == nullptr && root->right == nullptr) {
+            return v;
+        }
+        int s = 0;
+        if (root->left) {
+            s += f(root->left, v);
+        }
+        if (root->right) {
+            s += f(root->right, v);
+        }
+        return s;
+    }
+};
+
+int main() {
+    string problemName = "1022";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto tree = jread_binary_tree(line_in);
+        auto res = sol.sumRootToLeaf(tree[0]);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
