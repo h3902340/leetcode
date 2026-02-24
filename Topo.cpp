1bef56c7db7b635d7df26cdc69bf01a10d5717b4
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-02-24 10:04:43 +0800

    wrap Topo as class

--- Topo.cpp
+++ Topo.cpp
@@ -2,37 +2,44 @@
 using namespace std;
 
 const int N = 1e5;
-int deg[N];
-int nei[N];
-int topo[N];
-void init(int n) {
-    for (int i = 0; i < n; i++) {
-        nei[i] = 0;
+class Topo {
+   public:
+    int deg[N];
+    int nei[N];
+    void init(int n) {
+        for (int i = 0; i < n; i++) {
+            nei[i] = 0;
+        }
     }
-}
-
-int main() {
-    int n = 6;
-    vector<vector<int>> edges = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {1, 5}};
-    int m = edges.size();
-    init(n);
-    for (int i = 0; i < m; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
+    void addEdge(int u, int v) {
         deg[u]++;
         deg[v]++;
         nei[u] ^= v;
         nei[v] ^= u;
     }
+};
+Topo t;
+int topo[N];
+
+int main() {
+    int n = 6;
+    vector<vector<int>> edges = {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {1, 5}};
+    int m = edges.size();
+    t.init(n);
+    for (int i = 0; i < m; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        t.addEdge(u, v);
+    }
     int j = 0;
     for (int i = 0; i < n; i++) {
         int a = i;
-        while (deg[a] == 1) {
+        while (t.deg[a] == 1) {
             topo[j++] = a;
-            int p = nei[a];
-            nei[p] ^= a;
-            deg[p]--;
-            deg[a] = 0;
+            int p = t.nei[a];
+            t.nei[p] ^= a;
+            t.deg[p]--;
+            t.deg[a] = 0;
             a = p;
         }
     }
