5e60bd51d56a874b7faee0c77e714e6ad8ebdfb6
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-03-15 12:22:52 +0800

    wrap DSU into class

--- DSU.cpp
+++ DSU.cpp
@@ -2,44 +2,49 @@
 using namespace std;
 
 const int N = 1e5;
-int parent[N];
-int find(int u) {
-    int p = u;
-    while (parent[p] != -1) {
-        p = parent[p];
+class DSU {
+    int parent[N];
+
+   public:
+    void init(int n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+        }
     }
-    while (parent[u] != -1) {
-        int tmp = parent[u];
-        parent[u] = p;
-        u = tmp;
+    int find(int u) {
+        int p = u;
+        while (parent[p] != -1) {
+            p = parent[p];
+        }
+        while (parent[u] != -1) {
+            int tmp = parent[u];
+            parent[u] = p;
+            u = tmp;
+        }
+        return p;
     }
-    return p;
-}
-void unite(int u, int v) {
-    int a = find(u);
-    int b = find(v);
-    if (b != a) {
-        parent[b] = a;
+    void unite(int u, int v) {
+        int a = find(u);
+        int b = find(v);
+        if (b != a) {
+            parent[b] = a;
+        }
     }
-}
-void init(int n) {
-    for (int i = 0; i < n; i++) {
-        parent[i] = -1;
-    }
-}
+};
 
 int main() {
     int n = 10;
+    DSU dsu;
     vector<vector<int>> edges = {{3, 4}, {1, 0}, {2, 3}, {2, 1}};
     int m = edges.size();
-    init(n);
+    dsu.init(n);
     for (int i = 0; i < m; i++) {
         int u = edges[i][0];
         int v = edges[i][1];
-        unite(u, v);
+        dsu.unite(u, v);
     }
-    int a = find(0);
-    int b = find(4);
+    int a = dsu.find(0);
+    int b = dsu.find(4);
     printf("a==b: %d\n", a == b);  // a==b: 1
     return 0;
 }
