a659021a1a4b6e5f569c8875874ad3032c0e3d96
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-04-21 17:05:53 +0800

    3894 (0 ms)

---
+++ 3894.cpp
@@ -0,0 +1,61 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    string trafficSignal(int timer) {
+        if (timer == 0) {
+            return "Green";
+        }
+        if (timer == 30) {
+            return "Orange";
+        }
+        if (timer > 30 && timer <= 90) {
+            return "Red";
+        }
+        return "Invalid";
+    }
+};
+
+int main() {
+    string problemName = "3894";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto timer = jread_int(line_in);
+        auto res = sol.trafficSignal(timer);
+        getline(file_out, line_out);
+        auto ans = jread_string(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
