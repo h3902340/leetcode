fd5f716c8cd1539fbce77f951650aa3a57b196df
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-04-26 15:51:38 +0800

    3915 (172 ms) FenwickTree

--- 3915.cpp
+++ 3915.cpp
@@ -52,11 +52,12 @@ class SegmentTree {
                    query(mid + 1, y, i * 2 + 1, mid + 1, r));
     }
 };
-SegmentTree up;
-SegmentTree dn;
-ll uptmp[N];
-ll dntmp[N];
 class Solution {
+    SegmentTree up;
+    SegmentTree dn;
+    ll uptmp[N];
+    ll dntmp[N];
+
    public:
     long long maxAlternatingSum(vector<int>& nums, int k) {
         int n = nums.size();
@@ -86,10 +87,81 @@ class Solution {
     }
 };
 
+class FenwickTree {
+    ll tree[U + 1];
+    ll tree2[U + 1];
+    int sz;
+
+   public:
+    void init(int n) {
+        sz = n + 1;
+        for (int i = 1; i < sz; i++) {
+            tree[i] = 0;
+            tree2[i] = 0;
+        }
+    }
+    void updateup(int i, ll v) {
+        for (int j = i; j > 0; j -= (j & -j)) {
+            tree[j] = max(tree[j], v);
+        }
+    }
+    void updatedn(int i, ll v) {
+        for (int j = i; j < sz; j += (j & -j)) {
+            tree2[j] = max(tree2[j], v);
+        }
+    }
+    ll queryup(int l) {
+        ll s = 0;
+        for (int j = l; j < sz; j += (j & -j)) {
+            s = max(s, tree[j]);
+        }
+        return s;
+    }
+    ll querydn(int r) {
+        ll s = 0;
+        for (int j = r; j > 0; j -= (j & -j)) {
+            s = max(s, tree2[j]);
+        }
+        return s;
+    }
+};
+class Solution2 {
+    FenwickTree t;
+    ll uptmp[N];
+    ll dntmp[N];
+
+   public:
+    long long maxAlternatingSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            mx = max(mx, nums[i]);
+        }
+        ll res = 0;
+        for (int i = 0; i < k; i++) {
+            uptmp[i] = nums[i];
+            dntmp[i] = nums[i];
+            res = max(res, (ll)nums[i]);
+        }
+        t.init(mx);
+        for (int i = k; i < n; i++) {
+            t.updateup(nums[i - k], uptmp[i - k]);
+            t.updatedn(nums[i - k], dntmp[i - k]);
+            ll a = t.querydn(nums[i] - 1);
+            ll b = t.queryup(nums[i] + 1);
+            uptmp[i] = a + nums[i];
+            dntmp[i] = b + nums[i];
+            res = max(res, uptmp[i]);
+            res = max(res, dntmp[i]);
+        }
+        return res;
+    }
+};
+
 int main() {
     string problemName = "3915";
     auto begin = jtimer();
-    Solution sol;
+    Solution2 sol;
     ifstream file_in("testcases/" + problemName + "_in.txt");
     ifstream file_out("testcases/" + problemName + "_out.txt");
     bool allPass = true;
