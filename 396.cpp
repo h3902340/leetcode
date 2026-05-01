ec4c094eedb6145b7ec5da5a8035743a0fc5a956
Jia-Ho Jian <ygmdwwyg@gmail.com>
2026-05-01 14:09:07 +0800

    396 (7 ms)

---
+++ 396.cpp
@@ -0,0 +1,64 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution {
+   public:
+    int maxRotateFunction(vector<int>& nums) {
+        int n = nums.size();
+        int a = 0;
+        int s = 0;
+        for (int i = 0; i < n; i++) {
+            a += nums[i];
+            s += i * nums[i];
+        }
+        int res = s;
+        for (int i = 0; i < n - 1; i++) {
+            s = s - a + nums[i] * n;
+            res = max(res, s);
+        }
+        return res;
+    }
+};
+
+int main() {
+    string problemName = "396";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto nums = jread_vector(line_in);
+        auto res = sol.maxRotateFunction(nums);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
