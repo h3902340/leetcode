04b2c9488dc949dfa438c1a818d82b8f47455f98
Jia-Ho Jian <jia-hojian@Jia-Hos-MacBook-Pro.local>
2026-05-05 00:02:50 +0800

    48 (0 ms)

---
+++ 48.cpp
@@ -0,0 +1,73 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; i++)
+        {
+            for (int j = i; j < n - 1 - i; j++)
+            {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = t;
+            }
+        }
+    }
+};
+
+int main()
+{
+    string problemName = "48";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in))
+    {
+        auto matrix = jread_vector2d(line_in);
+        sol.rotate(matrix);
+        getline(file_out, line_out);
+        auto ans = jread_vector2d(line_out);
+        printf("Case %d", ++caseCount);
+        if (matrix == ans)
+        {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        }
+        else
+        {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(matrix, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass)
+    {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    }
+    else
+    {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
