a2551dd343f469085391e6ba8d52d66837fad118
Jia-Ho Jian <jia-hojian@Jia-Hos-MacBook-Pro.local>
2026-05-05 01:28:49 +0800

    843 (0 ms) (not always pass)

---
+++ 843.cpp
@@ -0,0 +1,101 @@
+#include "Jutil.h"
+using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+
+const int N = 100;
+bool vis[N];
+int adj[N][N];
+class Solution
+{
+public:
+    void findSecretWord(vector<string> &words, Master &master)
+    {
+        int n = words.size();
+        for (int i = 0; i < n; i++)
+        {
+            vis[i] = false;
+            for (int j = 0; j < n; j++)
+            {
+                adj[i][j] = 0;
+                for (int k = 0; k < 6; k++)
+                {
+                    if (words[i][k] == words[j][k])
+                    {
+                        adj[i][j]++;
+                    }
+                }
+            }
+        }
+        for (int t = 0; t < n; t++)
+        {
+            int i = rand() % n;
+            while (vis[i])
+            {
+                i = rand() % n;
+            }
+            int a = master.guess(words[i]);
+            if (a == 6)
+            {
+                break;
+            }
+            vis[i] = true;
+            for (int j = 0; j < n; j++)
+            {
+                if (adj[i][j] != a)
+                {
+                    vis[j] = true;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    string problemName = "843";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in))
+    {
+        auto grid = jread_vector2d(line_in);
+        auto res = sol.canPartitionGrid(grid);
+        getline(file_out, line_out);
+        auto ans = jread_int(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans)
+        {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        }
+        else
+        {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass)
+    {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    }
+    else
+    {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
+}
