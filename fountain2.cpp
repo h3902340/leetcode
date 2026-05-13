2bb7df92b321146f605cdf5405b4a2ddd1a6de0d
h3902340 <ygmdwwyg@gmail.com>
2026-05-13 13:00:52 +0800

    fix AI testcases

--- fountain2.cpp
+++ fountain2.cpp
@@ -1,155 +1,159 @@
-#include <iostream>
-#include <queue>
-#include <vector>
+#include "Jutil.h"
 using namespace std;
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
 
 struct Data {
     int i;
     int j;
 };
 const int dir[5] = {0, 1, 0, -1, 0};
-vector<vector<bool>> solve(vector<vector<int>>& height,
-                           vector<vector<int>>& fountain) {
-    int m = height.size();
-    int n = height[0].size();
-    int l = fountain.size();
-
-    vector<vector<bool>> res(m, vector<bool>(n, false));
-    vector<vector<int>> src(m, vector<int>(n, -1));
-    vector<int> par(l, -1);
-    queue<Data> q;
-    for (int i = 0; i < l; i++) {
-        int x = fountain[i][0];
-        int y = fountain[i][1];
-        q.push({x, y});
-        src[x][y] = i;
-        while (!q.empty()) {
-            Data u = q.front();
-            q.pop();
-            for (int k = 0; k < 4; k++) {
-                int x2 = u.i + dir[k];
-                int y2 = u.j + dir[k + 1];
-                if (x2 < 0 || y2 < 0 || x2 >= m || y2 >= n) {
-                    continue;
-                }
-                if (height[x2][y2] <= height[x][y]) {
-                    if (src[x2][y2] != -1) {
-                        if (src[x2][y2] != i) {
-                            par[i] = src[x2][y2];
-                        }
+class Solution {
+   public:
+    vector<vector<int>> solve(vector<vector<int>>& height,
+                              vector<vector<int>>& fountain) {
+        int m = height.size();
+        int n = height[0].size();
+        int l = fountain.size();
+        jprint(height, "height");
+        jprint(fountain, "fountain");
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        vector<vector<int>> src(m, vector<int>(n, -1));
+        vector<int> par(l, -1);
+        queue<Data> q;
+        for (int i = 0; i < l; i++) {
+            int x = fountain[i][0];
+            int y = fountain[i][1];
+            q.push({x, y});
+            src[x][y] = i;
+            while (!q.empty()) {
+                Data u = q.front();
+                q.pop();
+                for (int k = 0; k < 4; k++) {
+                    int x2 = u.i + dir[k];
+                    int y2 = u.j + dir[k + 1];
+                    if (x2 < 0 || y2 < 0 || x2 >= m || y2 >= n) {
                         continue;
                     }
-                    q.push({x2, y2});
-                    src[x2][y2] = i;
+                    if (height[x2][y2] <= height[x][y]) {
+                        if (src[x2][y2] != -1) {
+                            if (src[x2][y2] != i) {
+                                par[i] = src[x2][y2];
+                            }
+                            continue;
+                        }
+                        q.push({x2, y2});
+                        src[x2][y2] = i;
+                    }
                 }
             }
         }
-    }
-    vector<bool> isRoot(l, false);
-    vector<bool> check(l, false);
-    for (int i = 0; i < l; i++) {
-        if (check[i]) {
-            continue;
-        }
-        int x = fountain[i][0];
-        int y = fountain[i][1];
-        int t = i;
-        int s = i;
-        int mx = height[x][y];
-        while (par[t] != -1) {
+        vector<bool> isRoot(l, false);
+        vector<bool> check(l, false);
+        for (int i = 0; i < l; i++) {
+            if (check[i]) {
+                continue;
+            }
+            int x = fountain[i][0];
+            int y = fountain[i][1];
+            int t = i;
+            int s = i;
+            int mx = height[x][y];
+            while (par[t] != -1) {
+                check[t] = true;
+                t = par[t];
+                x = fountain[t][0];
+                y = fountain[t][1];
+                mx = max(mx, height[x][y]);
+            }
             check[t] = true;
-            t = par[t];
+            while (par[s] != -1) {
+                int a = par[s];
+                par[s] = t;
+                s = a;
+            }
             x = fountain[t][0];
             y = fountain[t][1];
-            mx = max(mx, height[x][y]);
+            if (height[x][y] < mx) {
+                res[x][y] = true;
+            }
+            height[x][y] = mx;
+            isRoot[t] = true;
         }
-        check[t] = true;
-        while (par[s] != -1) {
-            int a = par[s];
-            par[s] = t;
-            s = a;
-        }
-        x = fountain[t][0];
-        y = fountain[t][1];
-        if (height[x][y] < mx) {
-            res[x][y] = true;
-        }
-        height[x][y] = mx;
-        isRoot[t] = true;
-    }
-    vector<vector<bool>> vis(m, vector<bool>(n, false));
-    for (int i = 0; i < l; i++) {
-        if (!isRoot[i]) {
-            continue;
-        }
-        int x = fountain[i][0];
-        int y = fountain[i][1];
-        q.push({x, y});
-        vis[x][y] = true;
-        while (!q.empty()) {
-            Data u = q.front();
-            q.pop();
-            for (int k = 0; k < 4; k++) {
-                int x2 = u.i + dir[k];
-                int y2 = u.j + dir[k + 1];
-                if (x2 < 0 || y2 < 0 || x2 >= m || y2 >= n) {
-                    continue;
-                }
-                if (vis[x2][y2]) {
-                    continue;
-                }
-                vis[x2][y2] = true;
-                if (height[x2][y2] <= height[x][y]) {
-                    if (height[x2][y2] < height[x][y]) {
-                        res[x2][y2] = true;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        for (int i = 0; i < l; i++) {
+            if (!isRoot[i]) {
+                continue;
+            }
+            int x = fountain[i][0];
+            int y = fountain[i][1];
+            q.push({x, y});
+            vis[x][y] = true;
+            while (!q.empty()) {
+                Data u = q.front();
+                q.pop();
+                for (int k = 0; k < 4; k++) {
+                    int x2 = u.i + dir[k];
+                    int y2 = u.j + dir[k + 1];
+                    if (x2 < 0 || y2 < 0 || x2 >= m || y2 >= n) {
+                        continue;
                     }
-                    q.push({x2, y2});
+                    if (vis[x2][y2]) {
+                        continue;
+                    }
+                    vis[x2][y2] = true;
+                    if (height[x2][y2] <= height[x][y]) {
+                        if (height[x2][y2] < height[x][y]) {
+                            res[x2][y2] = true;
+                        }
+                        q.push({x2, y2});
+                    }
                 }
             }
         }
+        return res;
     }
-    return res;
-}
-
-void test(vector<vector<int>>& height, vector<vector<int>>& fountain,
-          vector<vector<bool>>& ans) {
-    vector<vector<bool>> res = solve(height, fountain);
-    cout << "res:[\n";
-    for (size_t i = 0; i < res.size(); i++) {
-        cout << "[";
-        for (size_t j = 0; j < res[i].size(); j++) {
-            cout << (res[i][j] ? 1 : 0) << ',';
-        }
-        cout << "]\n";
-    }
-    cout << "]\n";
-    cout << "ans:[\n";
-    for (size_t i = 0; i < ans.size(); i++) {
-        cout << "[";
-        for (size_t j = 0; j < ans[i].size(); j++) {
-            cout << (ans[i][j] ? 1 : 0) << ',';
-        }
-        cout << "]\n";
-    }
-    cout << "]\n";
-}
+};
 
 int main() {
-    // test1
-    vector<vector<int>> height = {
-        {1, 3, 1, 1}, {3, 3, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 2}};
-    vector<vector<int>> fountain = {{3, 3}};
-    vector<vector<bool>> ans = {
-        {0, 0, 1, 1}, {0, 0, 1, 1}, {1, 1, 0, 1}, {1, 1, 1, 0}};
-    test(height, fountain, ans);
-    // test2
-    height = {{1, 3, 1, 1}, {3, 3, 1, 1}, {1, 1, 2, 1}, {1, 1, 1, 4}};
-    fountain = {{0, 0}, {3, 3}};
-    ans = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 0}};
-    test(height, fountain, ans);
-    // test3
-    height = {{1, 5, 1, 2}, {3, 3, 5, 5}, {1, 1, 2, 1}, {1, 1, 1, 4}};
-    fountain = {{0, 0}, {3, 3}, {0, 3}};
-    ans = {{1, 0, 1, 1}, {1, 1, 0, 0}, {1, 1, 1, 1}, {1, 1, 1, 0}};
-    test(height, fountain, ans);
+    string problemName = "fountain2";
+    auto begin = jtimer();
+    Solution sol;
+    ifstream file_in("testcases/" + problemName + "_in.txt");
+    ifstream file_out("testcases/" + problemName + "_out.txt");
+    bool allPass = true;
+    int caseCount = 0;
+    int passCount = 0;
+    string line_in;
+    string line_out;
+    while (getline(file_in, line_in)) {
+        auto height = jread_vector2d(line_in);
+        getline(file_in, line_in);
+        auto fountain = jread_vector2d(line_in);
+        auto res = sol.solve(height, fountain);
+        getline(file_out, line_out);
+        auto ans = jread_vector2d(line_out);
+        printf("Case %d", ++caseCount);
+        if (res == ans) {
+            passCount++;
+            printf(" %s(PASS)", KGRN);
+        } else {
+            printf(" %s(WRONG)", KRED);
+            allPass = false;
+        }
+        printf("\n%s", KNRM);
+        jprint(res, "res");
+        jprint(ans, "ans");
+        printf("\n");
+    }
+    if (allPass) {
+        printf("%sALL CORRECT [%d/%d]\n%s", KGRN, passCount, caseCount, KNRM);
+    } else {
+        printf("%sWRONG ANSWER [%d/%d]\n%s", KRED, passCount, caseCount, KNRM);
+    }
+    auto end = jtimer();
+    jprint_time(begin, end);
+    return 0;
 }
